45be83251ce31605fe1fc3d2b5b803e5c3a79c78
Marian <mocanumarian1993@gmail.com>
2024-02-21 13:37:45 +0100

    fixed dump & import scripts

--- scripts/dump-database.sh
+++ scripts/dump-database.sh
@@ -2,7 +2,7 @@
 
 # This script is used to dump the database to a file
 
-source ../.env
+source .env
 
 CONTAINER_ID=$(docker ps -qf "name=postgresDB")
-docker exec -t $CONTAINER_ID pg_dump -U $POSTGRES_USERNAME -d $POSTGRES_NAME >db_dump.sql
+docker exec -t $CONTAINER_ID pg_dump --data-only -U $POSTGRES_USERNAME -d $POSTGRES_NAME >db_data.sql
--- scripts/import-database.sh
+++ scripts/import-database.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-# This script is used to import the database from a file
+source .env
 
-source ../.env
+CONTAINER_ID=$(docker ps -qf "name=cbs-postgresql")
 
-CONTAINER_ID=$(docker ps -qf "name=postgresDB")
+# Create a backup
+docker exec -t $CONTAINER_ID pg_dump --data-only -U $POSTGRES_USERNAME -d $POSTGRES_NAME > backup.sql
+
+# Truncate all tables
+docker exec -i $CONTAINER_ID psql -U $POSTGRES_USERNAME -d $POSTGRES_NAME <<EOF
+DO
+\$do\$
+DECLARE
+   _tbl text;
+BEGIN
+   FOR _tbl  IN
+      SELECT quote_ident(t.table_schema) || '.' || quote_ident(t.table_name)
+      FROM   information_schema.tables t
+      WHERE  t.table_schema NOT LIKE 'pg_%'
+      AND    t.table_schema != 'information_schema'
+      AND    t.table_type = 'BASE TABLE'
+   LOOP
+      EXECUTE 'TRUNCATE TABLE ' || _tbl || ' CASCADE';
+   END LOOP;
+END
+\$do\$;
+EOF
+
+# Import the data
 cat db_dump.sql | docker exec -i $CONTAINER_ID psql -U $POSTGRES_USERNAME -d $POSTGRES_NAME
